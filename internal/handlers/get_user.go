caf436b05958984d517b9d696135a36df2e78945
Cresten St. Clair <crestenn@gmail.com>
2023-10-19 14:06:35 -0700

    :lipstick:

--- internal/handlers/get_user.go
+++ internal/handlers/get_user.go
@@ -15,12 +15,12 @@ import (
 func makeResponse[T any](respBody T, statusCode int) events.APIGatewayProxyResponse {
 	var buf bytes.Buffer
 
-  body, _ := json.Marshal(respBody)
+	body, _ := json.Marshal(respBody)
 
 	json.HTMLEscape(&buf, body)
-  return events.APIGatewayProxyResponse{
-		StatusCode:      statusCode,
-		Body:            buf.String(),
+	return events.APIGatewayProxyResponse{
+		StatusCode: statusCode,
+		Body:       buf.String(),
 		Headers: map[string]string{
 			"Content-Type": "application/json",
 		},
@@ -28,36 +28,33 @@ func makeResponse[T any](respBody T, statusCode int) events.APIGatewayProxyRespo
 }
 
 func GetUser(ctx context.Context, request events.APIGatewayProxyRequest, crud *crud.Crud) (events.APIGatewayProxyResponse, error) {
-
 	ctx, cancel := context.WithTimeout(ctx, time.Duration(crud.Config.RequestTimeoutMS)*time.Millisecond)
 
 	defer cancel()
 
 	id := request.PathParameters["id"]
 	user, err := crud.Repo.GetUser(ctx, id)
-
 	if err != nil {
 		crud.Logger.Error("Failed to get user", zap.Error(err))
 
-    return makeResponse(map[string]string{
-      "error": "An internal error occured",
-    }, 500), nil
+		return makeResponse(map[string]string{
+			"error": "An internal error occured",
+		}, 500), nil
 	}
 
 	if user == nil {
 		crud.Logger.Error("User not found", zap.String("id", id))
 		return makeResponse(map[string]string{
-      "error": fmt.Sprintf("User not found. ID: %s", id),
-    }, 404), nil
+			"error": fmt.Sprintf("User not found. ID: %s", id),
+		}, 404), nil
 	}
 
 	if err != nil {
 		crud.Logger.Error("Failed to parse user after fetching from database", zap.Error(err))
 		return makeResponse(map[string]string{
-      "error": "An internal error occured",
-    }, 500), nil
+			"error": "An internal error occured",
+		}, 500), nil
 	}
-
 
 	return makeResponse(user, 200), nil
 }
