fb8768fe8c43f4ad0e595a8fe7a34061d872653a
Cresten St. Clair <crestenn@gmail.com>
2023-10-19 14:04:11 -0700

    refactored get_user

--- internal/handlers/get_user.go
+++ internal/handlers/get_user.go
@@ -12,60 +12,51 @@ import (
 	"go.uber.org/zap"
 )
 
+func makeResponse[T any](respBody T, statusCode int) events.APIGatewayProxyResponse {
+	var buf bytes.Buffer
+
+  body, _ := json.Marshal(respBody)
+
+	json.HTMLEscape(&buf, body)
+  return events.APIGatewayProxyResponse{
+		StatusCode:      statusCode,
+		Body:            buf.String(),
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+	}
+}
+
 func GetUser(ctx context.Context, request events.APIGatewayProxyRequest, crud *crud.Crud) (events.APIGatewayProxyResponse, error) {
+
 	ctx, cancel := context.WithTimeout(ctx, time.Duration(crud.Config.RequestTimeoutMS)*time.Millisecond)
 
 	defer cancel()
 
 	id := request.PathParameters["id"]
 	user, err := crud.Repo.GetUser(ctx, id)
+
 	if err != nil {
 		crud.Logger.Error("Failed to get user", zap.Error(err))
-		return events.APIGatewayProxyResponse{
-			StatusCode:      500,
-			IsBase64Encoded: false,
-			Body:            "An internal error has occured",
-			Headers: map[string]string{
-				"Content-Type": "application/json",
-			},
-		}, nil
 
+    return makeResponse(map[string]string{
+      "error": "An internal error occured",
+    }, 500), nil
 	}
 
 	if user == nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode:      404,
-			IsBase64Encoded: false,
-			Body:            fmt.Sprintf("User not found. ID: %s", id),
-			Headers: map[string]string{
-				"Content-Type": "application/json",
-			},
-		}, nil
+		return makeResponse(map[string]string{
+      "error": fmt.Sprintf("User not found. ID: %s", id),
+    }, 404), nil
 	}
 
-	body, err := json.Marshal(user)
 	if err != nil {
-		crud.Logger.Error("Failed to get user", zap.Error(err))
-		return events.APIGatewayProxyResponse{
-			StatusCode:      500,
-			IsBase64Encoded: false,
-			Body:            "An internal error has occured",
-			Headers: map[string]string{
-				"Content-Type": "application/json",
-			},
-		}, nil
-
+		crud.Logger.Error("Failed to parse user after fetching from database", zap.Error(err))
+		return makeResponse(map[string]string{
+      "error": "An internal error occured",
+    }, 500), nil
 	}
 
-	var buf bytes.Buffer
-	json.HTMLEscape(&buf, body)
 
-	return events.APIGatewayProxyResponse{
-		StatusCode:      200,
-		IsBase64Encoded: false,
-		Body:            buf.String(),
-		Headers: map[string]string{
-			"Content-Type": "application/json",
-		},
-	}, nil
+	return makeResponse(user, 200), nil
 }
